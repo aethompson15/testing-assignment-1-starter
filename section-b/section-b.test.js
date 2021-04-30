69fec843f3fb8b0317be92860783f024408e3a0a
aethompson15 <amelia.thompson@ada.ac.uk>
2021-04-30 16:23:14 +0100

    tests up to b12

--- section-b/section-b.test.js
+++ section-b/section-b.test.js
@@ -21,6 +21,17 @@ describe('Test b11, testing the largestValue() function', () => {
 });
 
 //Test B12
+describe('Test b12, testing the getEven() function', () => {
+    test("Testing using an array containing 2 odd and 2 even numbers", () => {
+        expect(getEven([1, 2, 3, 4])).toEqual([2, 4]);
+    });
+    test("Testing using an array with only even values", () => {
+        expect(getEven([2, 4, 6])).toEqual([2, 4, 6]);
+    });
+    test("Testing using an array with negetive values", () => {
+        expect(getEven([-1, -2, -3, -4])).toEqual([-2, -4]);
+    });
+});
 
 //Test B13
 
