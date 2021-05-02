e4145f2316b8ce1d8f8a1db367d63936bf0bad20
aethompson15 <amelia.thompson@ada.ac.uk>
2021-05-02 22:50:56 +0100

    tests workin up to c17 but not including b13

--- section-c/section-c.js
+++ section-c/section-c.js
@@ -2,21 +2,27 @@ function mysteryFunction(x) {
     // prime=true, not=false
     if(x === 2){
         return true;
-    }
+    };
     if(x < 2){
         return false;
-    }
+    };
     for(var i = 2; i < x; i++){
         if(x % i === 0) {
             return false;
-            break;
-        }
-    }
+        };
+    };
     return true;
 }
 
 function mysteryFunction2(y) {
-    // coding and coding here
+    // multiplying the value by all values preceeding it
+    if((y < 0) || (y > 12)){
+        throw new Error("Out of range!");
+    };
+    let result = y;
+    for(var i = y - 1; i <= 0; i--){
+        result += result * i;
+    };
     return;
 }
 
