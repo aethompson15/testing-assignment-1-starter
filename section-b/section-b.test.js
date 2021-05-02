21f416cab00a80f26fac2ac7bb55d8c7460e9a36
aethompson15 <amelia.thompson@ada.ac.uk>
2021-05-02 17:00:28 +0100

    tests working up to b14 - b13 not working yet

--- section-b/section-b.test.js
+++ section-b/section-b.test.js
@@ -33,8 +33,37 @@ describe('Test b12, testing the getEven() function', () => {
     });
 });
 
-//Test B13
+//Test B13 // function needs changing to not 
+/* describe('Test b13, testing the houseForSale() function', () => {
+    test("Testing the existing function properties", () => {
+        expect(houseForSale).toHaveReturnedWith('bath', true);
+        expect(houseForSale).toHaveReturnedWith('bedrooms', 4);
+        expect(houseForSale).toHaveReturnedWith('kitchen.amenities', ['oven', 'stove', 'washer']);
+        expect(houseForSale).toHaveReturnedWith('kitchen.area', 20);
+        expect(houseForSale).toHaveReturnedWith('kitchen.nice.oven');
+    });
+    test("Testing non-existent function properties", () => {
+        expect(houseForSale).not.toHaveReturnedWith('jacuzzi');
+        expect(houseForSale).not.toHaveReturnedWith('bedrooms', 10);
+        expect(houseForSale).not.toHaveReturnedWith('kitchen.amenities', ['hotplate', 'breakfast bar', 'pantry']);
+        expect(houseForSale).not.toHaveReturnedWith('kitchen.area', 50);
+        expect(houseForSale).not.toHaveReturnedWith('kitchen.big.garden');
+    });
+}); */
 
 //Test B14
+describe('Test b14, testing the sortNumbers() function', () => {
+    test('Testing an empty array', () => {
+        expect(sortNumbers([])).toEqual([]);
+        expect(sortNumbers([])).toHaveLength(0);
+    });
+    test('Testing an unordered array', () => {
+        expect(sortNumbers([5, 7, 9, 2, 6])).toEqual([2, 5, 6, 7 ,9]);
+        expect(sortNumbers([5, 7, 9, 2, 6])).toHaveLength(5);
+    });
+    test('Testing a non-numeric array', () => {
+        expect(sortNumbers(['b', 'd', 'a', 'c'])).not.toEqual(['a', 'b', 'c', 'd']);
+    })
+});
 
 //Test B15
